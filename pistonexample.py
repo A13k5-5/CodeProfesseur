3664c849ac28eff2bfee3e38605112e3672dc9dd
Philipp Bruhns <philipp.bruhns@protonmailcom>
2025-01-31 10:37:29 +0000

    Add Piston API example code

---
+++ pistonexample.py
@@ -0,0 +1,17 @@
+from pyston import PystonClient,File
+import asyncio
+
+async def main():
+
+    with open('test.py') as f:
+        file = File(f)
+
+    client = PystonClient()
+    output = await client.execute(
+        "python",
+        [file]
+    )
+
+    print(output)
+
+asyncio.get_event_loop().run_until_complete(main())
