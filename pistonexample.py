319b4d9cbd544bb20b3728578212efb96ac23e51
A13k5-5 <pisonalex@gmail.com>
2025-02-03 16:06:50 +0100

    Args test

--- pistonexample.py
+++ pistonexample.py
@@ -1,19 +1,18 @@
 # pip install aiopyston
 
-from pyston import PystonClient,File
+from pyston import PystonClient, File
 import asyncio
+
 
 async def main():
 
-    with open('test.py') as f:
+    with open("test.py") as f:
         file = File(f)
 
     client = PystonClient()
-    output = await client.execute(
-        "python",
-        [file]
-    )
+    output = await client.execute("python", [file], args=[123])
 
     print(output)
+
 
 asyncio.get_event_loop().run_until_complete(main())
--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("Test")
+import sys
+
+
+def get_sum(a):
+    print(a)
+
+
+# if __name__ == "__main__":
+#     num1 = sys.argv[1]
+#     num2 = sys.argv[2]
+#     num3 = sys.argv[3]
+#     print(get_sum(num1, num2))
