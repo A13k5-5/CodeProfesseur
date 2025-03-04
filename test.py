fc5cef32f25567bc4b5d21b0655a9adc9e02a6cb
A13k5-5 <pisonalex@gmail.com>
2025-03-04 12:33:22 +0100

    instead of printing now returns

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
-import sys
-
-
 def get_sum(a):
-    print(a)
+    return a
 
 
-# if __name__ == "__main__":
-#     num1 = sys.argv[1]
-#     num2 = sys.argv[2]
-#     num3 = sys.argv[3]
-#     print(get_sum(num1, num2))
+if __name__ == "__main__":
+    #     num1 = sys.argv[1]
+    #     num2 = sys.argv[2]
+    #     num3 = sys.argv[3]
+    #     print(get_sum(num1, num2))
+    print(get_sum(5))
