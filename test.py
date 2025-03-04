b78b6d090732a3fb73723abd60631ec8c41482a7
A13k5-5 <pisonalex@gmail.com>
2025-03-04 12:37:16 +0100

    Runs tests on a function

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
+import sys
+
+
 def get_sum(a):
     return a
 
 
 if __name__ == "__main__":
-    #     num1 = sys.argv[1]
-    #     num2 = sys.argv[2]
-    #     num3 = sys.argv[3]
-    #     print(get_sum(num1, num2))
-    print(get_sum(5))
+    if len(sys.argv) > 1:
+        value = int(sys.argv[1])
+        print(get_sum(value))
+    else:
+        print(get_sum(5))  # Default if no argument is provided
