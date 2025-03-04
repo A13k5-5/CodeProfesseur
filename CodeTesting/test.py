6990baf32a69057af6a57835e6d797adf44a9164
A13k5-5 <pisonalex@gmail.com>
2025-03-04 13:35:07 +0100

    Moved to CodeTesting directory

---
+++ CodeTesting/test.py
@@ -0,0 +1,7 @@
+def get_sum(a):
+    if a == 8:
+        return 7
+    return a
+
+if __name__ == "__main__":
+    print(get_sum(8))
---
+++ CodeTesting/test_wrapper.py
@@ -0,0 +1,12 @@
+from test import get_sum
+
+if __name__ == "__main__":
+    with open("tests.txt", "r") as tests_file:
+        test_cases = [int(line.strip()) for line in tests_file.readlines()]
+    with open("expectedOutput.txt", "r") as test_output_files:
+        expected_outputs = [int(line.strip()) for line in test_output_files.readlines()]
+    for i, test in enumerate(test_cases):
+        output = get_sum(test)
+        assert (
+            output == expected_outputs[i]
+        ), f"Incorrect for input: {test}, received: {output}, expected: {expected_outputs[i]}"
