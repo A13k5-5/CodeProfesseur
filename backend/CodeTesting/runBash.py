1433eaf697ae036cb026281803495eb12ea4346b
Alex Pison <pisonalex@gmail.com>
2025-03-24 16:13:39 +0100

    Now combines input and output jsons into one json

--- backend/CodeTesting/runBash.py
+++ backend/CodeTesting/runBash.py
@@ -1,12 +1,18 @@
 import subprocess
+import json
 
 
 def exec_bash(input_json, output_json, path):
+    input_data = json.loads(input_json)
+    output_data = json.loads(output_json)
+
+    combined_data = {"input": input_data, "output": output_data}
+
     with open(path, "w") as file:
-        file.write(input_json)
+        json.dump(combined_data, file)
     try:
         result = subprocess.run(
-            ["bash", "./run_tests.sh"],
+            ["bash", "run_tests.sh"],
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
