f264469b32c7400397a9abd11e89b8a99a1ef81f
Alex Pison <pisonalex@gmail.com>
2025-03-24 22:26:54 +0100

    removed annoyinh docker messages

--- backend/CodeTesting/run_tests.sh
+++ backend/CodeTesting/run_tests.sh
@@ -1,7 +1,7 @@
 cd CodeTesting
 
 # Build the Docker image
-docker build -q -t test-runner .
+docker build -q -t test-runner . > /dev/null
 
 # Run the Docker container
 docker run --rm -q test-runner
