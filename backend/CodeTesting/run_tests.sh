ab169f3a56f106338319ab9727d8656f157def1b
Alex Pison <pisonalex@gmail.com>
2025-03-24 20:37:51 +0100

    Changed comments in the bash file

--- backend/CodeTesting/run_tests.sh
+++ backend/CodeTesting/run_tests.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
 # Build the Docker image
 docker build -q -t test-runner .
 
 # Run the Docker container
 docker run --rm -q test-runner
-
-# The --rm flag automatically removes the container after it exits.
