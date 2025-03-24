ecc68ba2eb552c63bc60bf682f5cd17950a0d1ea
Alex Pison <pisonalex@gmail.com>
2025-03-24 15:12:25 +0100

    removed path argument from exec_bash

--- backend/CodeTesting/runBash.py
+++ backend/CodeTesting/runBash.py
@@ -1,14 +1,22 @@
 import subprocess
 
-def exec_bash(path: str):
+
+def exec_bash():
     try:
-        result = subprocess.run(['bash', path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        result = subprocess.run(
+            ["bash", "./run_tests.sh"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
         output = result.stdout
         return output
     except subprocess.CalledProcessError as e:
         return e.stderr
-    
-if __name__ == '__main__':
-    path = './run_tests.sh'
-    output = exec_bash(path)
+
+
+if __name__ == "__main__":
+    path = "./run_tests.sh"
+    output = exec_bash()
     print(output)
