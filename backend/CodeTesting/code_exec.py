545c161412432934ece5bdace823c3015810afdb
Alex Pison <pisonalex@gmail.com>
2025-03-28 11:18:38 +0100

    Refactoring

--- backend/CodeTesting/code_exec.py
+++ backend/CodeTesting/code_exec.py
@@ -7,7 +7,9 @@ from judge0 import File, Filesystem
 CPU_TIME_LIMIT = 3.0
 
 
-def write_sample_json(input_json, output_json, submission_path, func_name):
+# This function generates a sample json that contains all the data
+# test wrapper uses.
+def generate_sample_json(input_json, output_json, submission_path, func_name):
     input_data = json.loads(input_json)
     output_data = json.loads(output_json)
 
@@ -18,28 +20,25 @@ def write_sample_json(input_json, output_json, submission_path, func_name):
         "func_name": func_name,
     }
 
-    with open("./CodeTesting/src/sample.json", "w") as file:
-        sample_json = json.dumps(combined_data)
-        file.write(sample_json)
-
-    return sample_json
+    return json.dumps(combined_data)
 
 
 def evaluate_submission(input_json, output_json, submission_path, func_name):
-    sample_json = write_sample_json(input_json, output_json, submission_path, func_name)
+    sample_json = generate_sample_json(
+        input_json, output_json, submission_path, func_name
+    )
     submission_fileName = os.path.basename(submission_path)
 
     with open("./CodeTesting/src/test_wrapper.py", "r") as file:
         wrapper = file.read()
 
-    # Take the submitted file from uploades folder
+    # Take the submitted file from uploads folder
     with open(f"./CodeTesting/uploads/{submission_fileName}", "r") as file:
         solution = file.read()
 
     fs = Filesystem(
         content=[
             File(name="sample.json", content=sample_json),
-            # Renames any solution into solution.py
             File(name=submission_fileName, content=solution),
         ]
     )
